6bc91943d65f2ab57b7cf0d564a7c0341b61fd45
Juliana Cândido <j.candido@ufsc.br>
2026-08-04 16:50:58 -0300

    style: ajusta espaçamento da capa na versão mobile

--- components/SiteCover.tsx
+++ components/SiteCover.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function SiteCover() {
   return (
-    <section className="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
+    <section className="mx-auto max-w-7xl px-4 pt-4 sm:pt-6 md:pt-8 sm:px-6 lg:px-8">
       <div className="overflow-hidden rounded-3xl border border-white/10 bg-white/5 shadow-2xl">
         <Image
           src="/images/capa-tecnologia-e-acordes.png"
